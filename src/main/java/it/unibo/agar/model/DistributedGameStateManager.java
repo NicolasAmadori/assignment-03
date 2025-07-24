81578d95cb840d68d835f67c0e9d0b155413f8b7
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-07-24 17:54:42 +0200

    removed unused code

--- src/main/java/it/unibo/agar/model/DistributedGameStateManager.java
+++ src/main/java/it/unibo/agar/model/DistributedGameStateManager.java
@@ -9,11 +9,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class DistributedGameStateManager implements GameStateManager {
-    private static final double PLAYER_SPEED = 2.0;
-    private static final int MAX_FOOD_ITEMS = 150;
     private static final Random random = new Random();
     private World world;
-    private final Player initialPlayer;
     private final MainController mainController;
     private List<PlayerController> playerStubs;
     private final double speed;
@@ -23,7 +20,6 @@ public class DistributedGameStateManager implements GameStateManager {
 
     public DistributedGameStateManager(final World initialWorld, final Player initialPlayer, final MainController mainController, final List<PlayerController> playerStubs, final double speed) {
         this.world = initialWorld;
-        this.initialPlayer = initialPlayer;
         this.mainController = mainController;
         this.playerStubs = new CopyOnWriteArrayList<>(playerStubs);
         this.speed = speed;
@@ -140,70 +136,4 @@ public class DistributedGameStateManager implements GameStateManager {
                 .removeFoods(eatenFood)
                 .addFoods(newFoods);
     }
-
-//    private World moveAllPlayers(final World currentWorld) {
-//        final List<Player> updatedPlayers = currentWorld.getPlayers().stream()
-//                .map(player -> {
-//                    Position direction = playerDirections.getOrDefault(player.getId(), Position.ZERO);
-//                    final double newX = player.getX() + direction.x() * PLAYER_SPEED;
-//                    final double newY = player.getY() + direction.y() * PLAYER_SPEED;
-//                    return player.moveTo(newX, newY);
-//                })
-//                .collect(Collectors.toList());
-//
-//        return new World(currentWorld.getWidth(), currentWorld.getHeight(), updatedPlayers, currentWorld.getFoods());
-//    }
-
-
-
-//    private World handleEating(final World currentWorld) {
-//        final List<Player> updatedPlayers = currentWorld.getPlayers().stream()
-//                .map(player -> growPlayer(currentWorld, player))
-//                .toList();
-//
-//        final List<Food> foodsToRemove = currentWorld.getPlayers().stream()
-//                .flatMap(player -> eatenFoods(currentWorld, player).stream())
-//                .distinct()
-//                .toList();
-//
-//        final List<Player> playersToRemove = currentWorld.getPlayers().stream()
-//                .flatMap(player -> eatenPlayers(currentWorld, player).stream())
-//                .distinct()
-//                .toList();
-//
-//        return new World(currentWorld.getWidth(), currentWorld.getHeight(), updatedPlayers, currentWorld.getFoods())
-//                .removeFoods(foodsToRemove)
-//                .removePlayers(playersToRemove);
-//    }
-//
-//    private Player growPlayer(final World world, final Player player) {
-//        final Player afterFood = eatenFoods(world, player).stream()
-//                .reduce(player, Player::grow, (p1, p2) -> p1);
-//
-//        return eatenPlayers(world, afterFood).stream()
-//                .reduce(afterFood, Player::grow, (p1, p2) -> p1);
-//    }
-//
-//    private List<Food> eatenFoods(final World world, final Player player) {
-//        return world.getFoods().stream()
-//                .filter(food -> EatingManager.canEatFood(player, food))
-//                .toList();
-//    }
-//
-//    private List<Player> eatenPlayers(final World world, final Player player) {
-//        return world.getPlayersExcludingSelf(player).stream()
-//                .filter(other -> EatingManager.canEatPlayer(player, other))
-//                .toList();
-//    }
-
-//    private void cleanupPlayerDirections() {
-//        List<String> currentPlayerIds = this.world.getPlayers().stream()
-//                .map(Player::getId)
-//                .collect(Collectors.toList());
-//
-//        this.playerDirections.keySet().retainAll(currentPlayerIds);
-//        this.world.getPlayers().forEach(p ->
-//                playerDirections.putIfAbsent(p.getId(), Position.ZERO));
-//    }
-
 }
